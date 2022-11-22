26303052129b8cc1ab41a0a9220c4fec1f0515db
jaz-code <1430715738@qq.com>
2022-11-22 20:53:44 +0800

    服务端（ant问题未解决）

--- src/index.tsx
+++ src/index.tsx
@@ -2,15 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import {loadDevTools } from "jira-dev-tool"
+loadDevTools(()=>{
+  const root = ReactDOM.createRoot(
+    document.getElementById('root') as HTMLElement
+  );
+  root.render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  );
+})
 
-
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
 
 
