2cb7d6e761a1d8f342c00a1f0499c0509987ffc8
jaz-code <1430715738@qq.com>
2022-11-22 15:37:17 +0800

    any->unkonwn

--- src/utilis/index.ts
+++ src/utilis/index.ts
@@ -1,7 +1,7 @@
 import { useEffect, useState } from "react"
 //出现0的情况时转为true不是就为value
-export const isFalsy = (value:any)=>(value===0?false:!value)
-export const cleanObject = (object:object) =>{
+export const isFalsy = (value:unknown)=>(value===0?false:!value)
+export const cleanObject = (object:object):any =>{
     const result = {...object}
     Object.keys(result).forEach(key=>{
         //获取value的值
