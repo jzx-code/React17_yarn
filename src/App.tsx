b61791ed32bd78423c77f33656d0aa0856d86c16
jaz-code <1430715738@qq.com>
2022-11-22 17:06:25 +0800

    json登录

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
-import Index from 'screens/project-list';
+import ProjectListScreen from 'screens/project-list';
+import LoginScreen from 'screens/login'
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <Index/>
+      <LoginScreen/>
     </div>
   );
 }
