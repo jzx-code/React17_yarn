65a1de7f11af9edf16ff750c392ec4d00990534a
jaz-code <1430715738@qq.com>
2022-11-23 21:33:08 +0800

    配置loadDevTools失败拉去远程序

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
-import ProjectListScreen from 'screens/project-list';
-import LoginScreen from 'screens/login'
-import './App.css';
+import React from "react";
+import "./App.css";
+import { ProjectListScreen } from "screens/project-list";
+import { LoginScreen } from "screens/login";
 
 function App() {
   return (
     <div className="App">
-      <LoginScreen/>
+      <LoginScreen />
+      {/*<ProjectListScreen />*/}
     </div>
   );
 }
